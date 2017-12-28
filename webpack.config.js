d1ba3ebe84fc83f64d9ede0bfacf0b5123beb9ff
Mr.Xi <xxy380491563@163.com>
2017-12-29 01:17:22 +0800

    webpack 配置

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+//__dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry:  __dirname + "/src/app/main.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+    },
+
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env", "react"
+                        ]
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true, // 指定启用css modules
+                            localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+
+};
+
