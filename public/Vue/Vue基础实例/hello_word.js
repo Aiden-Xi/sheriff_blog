284c1fbe15f99db56f10c5692793927985c57682
Mr.Xi <xxy380491563@163.com>
2018-03-29 22:20:28 +0800

    fix

--- public/Vue/Vue基础实例/hello_word.js
+++ public/Vue/Vue基础实例/hello_word.js
@@ -3,7 +3,9 @@ new Vue({
     data: {
         message: 'Hello Word!!!',
         rawHtml: '<span style="color: #fe23e3;">生命周期3333</span>',
-        rawHtml2: '<span style="font-size: 3em; color: #3e8e41;">生命周期444444</span>'
+        rawHtml2: '<span style="font-size: 3em; color: #3e8e41;">生命周期444444</span>',
+        isActive: true,
+        title: '请点击我！！！'
     },
     methods: {
         clickSpan: function () {
